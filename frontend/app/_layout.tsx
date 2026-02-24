8f579f976825d45999329a877d2947ba5c22ba6e
Berlindraft <xraymundzyron@gmail.com>
2026-02-24 15:16:22 +0800

    chore: consolidate recent development, conflict resolutions, and tracking fixes

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -4,8 +4,10 @@ import "../global.css";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" />
+      <Stack.Screen name="login-and-authetication/login" />
+      <Stack.Screen name="login-and-authetication/signup" />
     </Stack>
   );
 }
