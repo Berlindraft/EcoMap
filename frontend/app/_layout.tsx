ddc76734a76c9d108208a3106847472f8f38ee09
Dan <dxn.xnt@gmail.com>
2026-02-24 11:12:02 +0800

    Added landing, login, and signup pages

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -6,6 +6,8 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="login" options={{ headerShown: false }} />
+      <Stack.Screen name="signup" options={{ headerShown: false }} />
     </Stack>
   );
 }
